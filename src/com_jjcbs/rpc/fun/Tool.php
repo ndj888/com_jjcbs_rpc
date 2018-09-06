dd2f93eda1daa7d0dcc756becaec28f42a7551c1
longbob <710190609@qq.com>
2018-09-06 13:26:49 +0800

    修改json 判断方法 可能影响服务

--- src/com_jjcbs/rpc/fun/Tool.php
+++ src/com_jjcbs/rpc/fun/Tool.php
@@ -11,15 +11,9 @@ namespace com_jjcbs\rpc\fun;
 
 class Tool
 {
-    public static function is_json($json_str) {
-        $json_str = str_replace('＼＼', '', $json_str);
-        $out_arr = array();
-        preg_match('/{.*}/', $json_str, $out_arr);
-        if (!empty($out_arr)) {
-            $result = json_decode($out_arr[0], TRUE);
-        } else {
-            return false;
-        }
-        return $result;
+    public static function is_json($json_str)
+    {
+        json_decode($json_str);
+        return (json_last_error() == JSON_ERROR_NONE);
     }
 }
